7f485f84740976d280f60dc9c6cc23bc7d50fea5
jeansuh42 <jeansuh42@gmail.com>
2024-10-10 02:23:09 +0900

    fix: Payment 통신 / Persistence 통신 분리

--- incer-paygate/src/main/java/incerpay/paygate/domain/component/PaymentPersistenceAdapter.java
+++ incer-paygate/src/main/java/incerpay/paygate/domain/component/PaymentPersistenceAdapter.java
@@ -1,17 +1,76 @@
 package incerpay.paygate.domain.component;
 
-import incerpay.paygate.presentation.dto.in.PaymentApproveCommand;
-import incerpay.paygate.presentation.dto.in.PaymentCancelCommand;
-import incerpay.paygate.presentation.dto.in.PaymentRejectCommand;
-import incerpay.paygate.presentation.dto.in.PaymentRequestCommand;
+import incerpay.paygate.infrastructure.internal.IncerPaymentApi;
+import incerpay.paygate.infrastructure.internal.dto.IncerPaymentApiView;
+import incerpay.paygate.presentation.dto.in.*;
 import incerpay.paygate.presentation.dto.out.PersistenceView;
+import org.springframework.stereotype.Component;
 
-public interface PaymentPersistenceAdapter {
+import java.util.UUID;
 
-    PersistenceView request(PaymentRequestCommand paymentRequestCommand);
-    PersistenceView cancel(PaymentCancelCommand paymentCancelCommand);
-    PersistenceView approve(PaymentApproveCommand paymentApproveCommand);
-    PersistenceView reject(PaymentRejectCommand paymentRejectCommand);
+@Component
+public class PaymentPersistenceAdapter {
+
+    private final IncerPaymentApi incerPaymentApi;
+    private final IncerPaymentApiMapper incerPaymentApiMapper;
+
+    public PaymentPersistenceAdapter(IncerPaymentApi incerPaymentApi,
+                                     IncerPaymentApiMapper incerPaymentApiMapper) {
+        this.incerPaymentApi = incerPaymentApi;
+        this.incerPaymentApiMapper = incerPaymentApiMapper;
+    }
+
+
+    public PersistenceView request(PaymentRequestCommand paymentRequestCommand) {
+        IncerPaymentApiRequestCommand command = incerPaymentApiMapper.toApiRequestCommand(paymentRequestCommand);
+        IncerPaymentApiView view = incerPaymentApi.request(command);
+
+        return new PersistenceView(
+                UUID.randomUUID(),
+                UUID.randomUUID(),
+                command.sellerId(),
+                view.state(),
+                0L
+        );
+    }
+
+
+    public PersistenceView cancel(PaymentCancelCommand paymentCancelCommand) {
+        IncerPaymentApiCancelCommand command = incerPaymentApiMapper.toApiCancelCommand(paymentCancelCommand);
+        IncerPaymentApiView view = incerPaymentApi.cancel(command);
+        return new PersistenceView(
+                view.paymentId(),
+                paymentCancelCommand.transactionId(),
+                view.sellerId(),
+                view.state(),
+                view.amount()
+        );
+    }
+
+
+    public PersistenceView reject(PaymentRejectCommand paymentRejectCommand) {
+        IncerPaymentApiRejectCommand command = incerPaymentApiMapper.toApiRejectCommand(paymentRejectCommand);
+        IncerPaymentApiView view = incerPaymentApi.reject(command);
+        return new PersistenceView(
+                view.paymentId(),
+                paymentRejectCommand.transactionId(),
+                view.sellerId(),
+                view.state(),
+                view.amount()
+        );
+    }
+
+
+    public PersistenceView approve(PaymentApproveCommand paymentApproveCommand) {
+        IncerPaymentApiApproveCommand command = incerPaymentApiMapper.toApiApproveCommand(paymentApproveCommand);
+        IncerPaymentApiView view = incerPaymentApi.approve(command);
+        return new PersistenceView(
+                view.paymentId(),
+                paymentApproveCommand.transactionId(),
+                paymentApproveCommand.customerId(),
+                view.state(),
+                view.amount()
+        );
+    }
 
 }
-
